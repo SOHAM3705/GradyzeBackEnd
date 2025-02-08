751c1c956b5ba776000ba58845394e38838af0e5
SOHAM3705 <sohampachpute05@gmail.com>
2025-02-08 11:25:48 +0530

    updated

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ web.get('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5173;
-app.listen(PORT, "0.0.0.0", () => {
+web.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
 
