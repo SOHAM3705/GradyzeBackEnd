e4ad1011991aa86093507d0817113f42ed47b6dc
SOHAM3705 <sohampachpute05@gmail.com>
2025-03-27 15:29:22 +0530

    student Notification

--- api/Password/passwordResetRoutes.js
+++ api/Password/passwordResetRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../../models/useradmin");
 const crypto = require("crypto");
 const { resetPasswordEmail } = require("../utils/resetPasswordEmail");
 const { Resend } = require("resend");
