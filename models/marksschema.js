256c811f3aa94286cdc6419cc568f36fdde0b4c4
SOHAM3705 <sohampachpute05@gmail.com>
2025-04-06 19:12:09 +0530

    Changes made in marks

--- models/marksschema.js
+++ models/marksschema.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const marksSchema = new mongoose.Schema({
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
+  studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Students', required: true },
   examType: { type: String, required: true },
   year: { type: String, required: true },
   exams: [
     {
       subjectName: { type: String, required: true },
-      teacherId: { type: mongoose.Schema.Types.ObjectId, ref: 'Faculty', required: true }, // moved here
+      teacherId: { type: mongoose.Schema.Types.ObjectId, ref: 'teachers', required: true }, // moved here
       marksObtained: { type: Number, required: true },
       totalMarks: { type: Number, required: true },
       status: { type: String, enum: ['Pass', 'Fail', 'Absent'], required: true },
