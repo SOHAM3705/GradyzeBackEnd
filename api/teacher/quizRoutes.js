ce4fbf7cbf7a3a47803dfeb893de24fa4d3027dc
SOHAM3705 <sohampachpute05@gmail.com>
2025-04-14 00:12:41 +0530

    Changes made in admin auth

--- api/teacher/quizRoutes.js
+++ api/teacher/quizRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const Quiz = require('../../models/quiz'); // Make sure this path is correct
+const Quiz = require('../../models/quiz.js'); // Make sure this path is correct
 
 // Create a new quiz
 router.post('/', async (req, res) => {
   try {
     const newQuiz = new Quiz(req.body);
     await newQuiz.save();
-    res.status(201).json(newQuiz);
+    res.status(201).json(newQuiz);a
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
